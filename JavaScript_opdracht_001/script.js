9717236273a76db7310f19db562425ad68a3017f
MarloesVeger <89524035+MarloesVeger@users.noreply.github.com>
2021-09-17 08:42:12 +0200

    Add files via upload

---
+++ JavaScript_opdracht_001/script.js
@@ -0,0 +1,15 @@
+<script>
+    console.log("Hello Winc Academy");
+    let name = 'Marloes';
+    console.log("Marloes")
+    console.log("8")
+    console.log("44")
+    console.log("3")
+    console.log("2")
+    console.log("16")
+    console.log("2")
+    console.log("1")
+    console.log("number")
+    let age = "45";
+    console.log("string")
+</script>
